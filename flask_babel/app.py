180590972a41d6d9dde1ff771c6bbd9817771b72
sfkara <seriffiratkara@gmail.com>
2022-01-04 11:30:36 +0300

    babel date, datetime and locale

---
+++ flask_babel/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel, get_locale, format_date, format_datetime
+from datetime import date, datetime
+
+
+app = Flask(__name__,template_folder='template')
+babel = Babel(app)
+
+
+@babel.localeselector
+def localeselector():
+    # return 'tr'
+    return request.accept_languages.best_match(["en_US", "es_ES"])
+
+
+@app.route("/")
+def index():
+    # return '<h1>Locale: {}</h1>'.format(get_locale())
+    d = date(2022, 1, 9)
+    dt = datetime(2023, 5, 19, 17, 30)
+
+    local_date = format_date(d)
+    local_datetime = format_datetime(dt, "short")
+
+    return render_template(
+        "index.html", Locale=get_locale(), Local_date=local_date, Local_datetime=local_datetime)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
