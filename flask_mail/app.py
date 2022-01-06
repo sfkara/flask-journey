c93bcda43af57436ce098d84f99c465780d2b3bc
sfkara <seriffiratkara@gmail.com>
2022-01-06 17:48:22 +0300

    flask-mail init

---
+++ flask_mail/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_mail import Mail
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['MAIL_SERVER'] = 'smtp.fastmail.com'
+    app.config['MAIL_PORT'] = 456
+    app.config['MAIL_USERNAME'] = 'seriffiratkara@gmail.com'
+    app.config['MAIL_PASSWORD'] = ''
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_DEFAULT_SENDER'] = 'seriffiratkara@gmail.com'
+    
+    
+    @app.route('/')
+    def index():
+        msg = Message('hello',recipients=['sfkara@gmail.com'])
+        mail.send(msg)
+        
+        return '<h1>Sent!!!!</h1>'
+    
+    
+    mail = Mail()
+    mail.init_app(app)
