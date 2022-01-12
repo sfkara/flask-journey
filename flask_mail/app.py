ce8c9ad7c7957c4704512b5e12954c0bdf5d7d94
sfkara <seriffiratkara@gmail.com>
2022-01-12 10:36:22 +0300

    add bulk mail

--- flask_mail/app.py
+++ flask_mail/app.py
@@ -12,6 +12,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_DEFAULT_SENDER'] = 'seriffiratkara@gmail.com'
+    app.config['MAIL_MAX_EMAIL'] = 26
     
     
     mail = Mail()
@@ -20,9 +21,19 @@ def create_app():
     @app.route('/')
     def index():
         msg = Message('hello',recipients=['sfkara@gmail.com'])
+        msg.add_recipients(recipients=['fk@gmail.com'])
+        msg.html = 'sent'
         mail.send(msg)
         
         return '<h1>Sent!!!!</h1>'
-    
+    @app.route('/bulk')
+    def bulk():
+        users = [{'name':'Fırat','email':'kara@gmail.com'}]
+        
+        with mail.connect() as connect:
+            for user in users:
+                msg = Message('Bulk',recipients=[user['email']])
+                msg.body = 'hello {}'.format(user['name'])
+                connect.send(msg)
     
     return app
